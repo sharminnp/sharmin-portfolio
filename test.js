a6fd83e2c0778171fbceb88cb7c6a509ca63b6c3
sharminnp <sharminnp123@gmail.com>
2022-08-12 20:02:25 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+// function validateform{
+//     var name=document.php-email-form.name.value;
+//     var subject=document.php-email-form.subject.value;
+//     // var message=document.php-email-form.meassage.value;
+//     if(name==null||name==""){
+//         alert('Name box not be blank')
+
+//     }else if()
+//     {
+
+//     }
+
+// }
